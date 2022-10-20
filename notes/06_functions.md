1f42ca383c63de7a52963697ac31a93152ef5272
jacobbumgarner <jacobbum21@gmail.com>
2022-10-20 17:40:50 -0400

    functions workthrough

--- notes/06_functions.md
+++ notes/06_functions.md
@@ -26,3 +26,34 @@ with other objects.
 ### 6.2.4 Invoking functions
 
 ## 6.3 Function composition
+Introduction to the binary operator `%>%`, which allows for easy compounding of functions.
+
+## 6.4 Lexical scoping
+R lexical scoping follows four primary rules:
+
+**Name masking**
+
+Names defined in a function mask names defined outside of a function.
+
+However, names defined outside of a function can be used inside of a function, even
+if they're not passed.
+
+**Functions versus variables**
+
+Stupid, not covering. Don't reuse object names...
+
+**A fresh start**
+
+Variables inside of functions do not exist within the function between calls. See example
+
+**Dynamic lookup**
+
+Functions use the most recently defined variable if a global variable is used inside
+of the function without being passed
+
+## 6.5 Lazy evaluation
+R functions are only evaluated when accessed.
+
+## 6.6 ...
+`...` is equivalent to `*args`.
+
--- scripts/06_functions.R
+++ scripts/06_functions.R
@@ -15,7 +15,7 @@ do.call(mean, args)
 x <- runif(100)
 
 # find standard deviation
-square <- function(x) x ^ 2
+square <- function(x) x^2
 deviation <- function(x) x - mean(x)
 
 sqrt(mean(square(deviation(x))))
@@ -25,9 +25,76 @@ out <- square(out)
 out <- mean(out)
 sqrt(out)
 
+# use the binary operator
 x %>%
     deviation() %>%
     square() %>%
     mean() %>%
     sqrt()
 sd(x)
+
+
+# 6.4 lexical scoping
+# name masking
+x <- 10
+
+g01 <- function() {
+    x
+}
+
+g01()
+
+x <- 1
+
+g02 <- function() {
+    y <- 2
+    i <- function() {
+        z <- 3
+        c(x, y, z)
+    }
+    i()
+}
+g02()
+
+# 6.4.3
+fresh_start <- function() {
+    if (!exists("a")) {
+        a <- 1
+    } else {
+        a <- a + 1
+    }
+    a
+}
+fresh_start()
+
+# Dynamic lookup
+x <- 10
+
+g03 <- function() x + 1
+g03()
+
+x <- 5
+g03()
+
+# Lazy evaluation
+h01 <- function(x) {
+    10
+}
+h01(stop("This is an error"))
+
+# 6.5.1 promises
+y <- 10
+
+h02 <- function(x, y) {
+    y <- 100
+    x + 1
+}
+
+h02(y)
+
+# default arguments
+x <- 1
+h <- function(x = ls()) {
+    a <- 1
+    x
+}
