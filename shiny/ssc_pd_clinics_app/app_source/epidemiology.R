0eaa61cbde80e416dc9de4f4163fbeb66808d882
jacobbumgarner <jacobbum21@gmail.com>
2022-11-29 17:56:50 -0500

    epidemiology page

--- shiny/ssc_pd_clinics_app/app_source/epidemiology.R
+++ shiny/ssc_pd_clinics_app/app_source/epidemiology.R
@@ -21,6 +21,28 @@ pm_sex_ratio <- c(pm_sex_ratio * 100, (1 - pm_sex_ratio) * 100)
 labels <- c("Women", "Men")
 
 
+## Layout
+# Title & text
+title <- fluidRow(box(
+    title = tags$h2("dcSSc & PM Epidemiology", align = "center"),
+    width = 12,
+    height = "50%",
+    tags$h4(
+    HTML(
+        "
+        Both of these diseases are extremely rare.
+        Importantly, there is a stark sex difference in the prevalence
+        of each disease, with females developing both dcSSc
+        <a target='_blank' href='https://pubmed.ncbi.nlm.nih.gov/34782448/'>[1]</a>
+        and PM
+        <a target='_blank' href='https://www.ncbi.nlm.nih.gov/books/NBK532860/#:~:text=The%20estimated%20prevalence%20of%20polymyositis,increase%20in%20the%20detection%20rate.'>[2]</a>
+        more frequently than males.
+        "
+    ),
+    align = "center"
+)
+))
+
 # Plot generation
 generate_bar_plot <- function(data, colors) {
     graph <- plot_ly(
@@ -47,9 +69,9 @@ generate_pie_chart <- function(data, colors) {
     graph
 }
 
-generate_plot <- function(graph_type, dataset, women_color, men_color) {
+generate_epidemiology_plot <- function(graph_type, dataset, women_color, men_color) {
     # Get data
-    colors <- c(women_color, men_color)    
+    colors <- c(women_color, men_color)
     graph_type <- tolower(graph_type)
     data <- if (tolower(dataset) == "dcssc") {
         dcssc_sex_ratio
@@ -70,7 +92,7 @@ generate_plot <- function(graph_type, dataset, women_color, men_color) {
             graph types for this function."
         )
     }
-    
+
     # Add boiler plate
     graph <- graph %>% layout(
         title = paste0("Sex differences in ", dataset, " prevalence."),
@@ -84,7 +106,7 @@ generate_plot <- function(graph_type, dataset, women_color, men_color) {
 # dcSSc Epidemiology
 dcssc_prevalence_text <- infoBox(
     "Population Prevalence",
-    "12.7 out of 100,000 people have dcSSc.",
+    "12.7 out of 100,000 people have dcSSc [3].",
     icon = icon("person-dress-burst", ),
     color = "purple",
     width = 12,
@@ -104,7 +126,7 @@ dcssc_box <- box(
 # PM Epidemiology
 PM_prevalence_text <- infoBox(
     "Population Prevalence",
-    "13.5 out of 100,000 people have PM.",
+    "13.5 out of 100,000 people have PM [2].",
     icon = icon("person-dress-burst", ),
     color = "purple",
     width = 12,
@@ -132,7 +154,7 @@ male_color_picker <- colourInput("men_color", "Select Men Color:", "#2986cc", )
 
 options_box <- box(
     title = "Graph Options",
-    width = 4,
+    width = 12,
     # status = "black",
     solidHeader = FALSE,
     icon = icon("gear"),
@@ -142,11 +164,29 @@ options_box <- box(
     male_color_picker
 )
 
+# References
+references <- box(
+    title = "References",
+    icon = icon("book"),
+    status = "black",
+    width = 12,
+    align = "left",
+    tags$ol(
+        tags$li("De Angelis, R., Giuggioli, D., Bajocchi, G., Dagna, L., Zanframundo, G., Foti, R., Cacciapaglia, F., Cuomo, G., Ariani, A. & Rosato, E. (2022) Sex-related differences in systemic sclerosis: a multicenter cross-sectional study from the National Registry of the Italian Society for Rheumatology. The Journal of Rheumatology. 49, 176-185."),
+        tags$li("Mammen, A.L. (2016) Autoimmune myopathies. CONTINUUM: Lifelong Learning in Neurology. 22, 1852."),
+        tags$li("Wibianto, A. & Santoso, I.D. (2020) Diffuse Cutaneous Systemic Scleroderma with Secondary In-fection: A Case Report. J Dermatol Res Ther. 6, 091.")
+    )
+)
+
 epidemiology_tab <- tabItem(
     tabName = "epidemiology",
-    tags$h2("Epidemiology", align = "center"),
+    title,
     fluidRow(
         dcssc_box, pm_box
     ),
-    fluidRow(column(12, options_box, offset = 4))
+    fluidRow(
+        column(3, options_box, offset = 1),
+        column(7, references),
+        # align = "center"
+    ),
 )
