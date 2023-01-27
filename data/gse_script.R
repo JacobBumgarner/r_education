dae7c36ef2c8a65d09ecbeb94b184e6e479cbf0a
jacobbumgarner <jacobbum21@gmail.com>
2023-01-27 17:45:42 -0500

    rentrez tutorial

---
+++ data/gse_script.R
@@ -0,0 +1,46 @@
+
+link_basename <- "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc="
+
+study_ids <- c(
+    "GSE15602",
+    "GSE24742",
+    "GSE45867",
+    "GSE65127",
+    "GSE66321",
+    "GSE68215",
+    "GSE72326",
+    "GSE78068",
+    "GSE91079",
+    "GSE112943",
+    "GSE117769",
+    "GSE148346",
+    "GSE151161",
+    "GSE172188"
+)
+
+metadata <- data.frame(
+    study_id = character(),
+    title = character(),
+    summary = character(),
+    link = character()
+)
+
+for (study_id in study_ids) {
+    print(study_id)
+    search_term <- paste0(study_id, "[ACCN] AND GSE[ETYP]")
+    id <- rentrez::entrez_search(
+        db = "gds",
+        term = search_term
+    )$id
+
+    result <- rentrez::entrez_summary(db = "gds", id = id)
+    data <- list(
+        study_id,
+        result$title,
+        result$summary,
+        paste0(link_basename, study_id)
+    )
+
+    metadata[nrow(metadata) + 1, ] <- data
+}
+metadata$summary
